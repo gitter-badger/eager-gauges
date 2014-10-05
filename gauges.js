5cc02f6a7a7091b7fef56990256c54f00ef61480
Nathaniel Blackburn <nblackburn@fastmail.fm>
2014-10-05 18:03:50 +0100

    Initial release

---
+++ gauges.js
@@ -0,0 +1,15 @@
+window.Gauges = {
+  init: function(options) {
+	var _gauges = _gauges || [];
+	(function() {
+		var gauges   = document.createElement('script');
+		gauges.type  = 'text/javascript';
+		gauges.async = options.asynchronous;
+		gauges.id    = 'gauges-tracker';
+		gauges.setAttribute('data-site-id', options.id);
+		gauges.src = '//secure.gaug.es/track.js';
+		var source = document.getElementsByTagName('script')[0];
+		source.parentNode.insertBefore(gauges, source);
+	})();
+  }
+};
